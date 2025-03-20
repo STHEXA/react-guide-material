ba55111a09090eb6a375dc16e840f7222f9df88a
STHEXA <ti.syu.s@gmail.com>
2025-03-20 15:05:19 +0900

    取得したデータを画面に反映

--- 16_rest_api/src/050_axios_get_state/start/Example.jsx
+++ 16_rest_api/src/050_axios_get_state/start/Example.jsx
@@ -1,5 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Example = () => {
-  return <></>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get("http://127.0.0.1:3003/user");
+      setUsers(res.data);
+    };
+    getUser();
+  }, []);
+
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h3>{user.username}</h3>
+            <p>age:{user.age}</p>
+            <p>hobbies:{user.hobbies.join(",")}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Example;
