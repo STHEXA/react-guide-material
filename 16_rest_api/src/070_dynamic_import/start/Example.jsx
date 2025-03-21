1a9d4aa74d113d2386ae16674b26848895140e76
STHEXA <ti.syu.s@gmail.com>
2025-03-21 22:48:41 +0900

    ダイナミックインポートを使用

--- 16_rest_api/src/070_dynamic_import/start/Example.jsx
+++ 16_rest_api/src/070_dynamic_import/start/Example.jsx
@@ -1,5 +1,18 @@
+// これはstaticインポート
+import { add } from "./add";
+
 const Example = () => {
-  
+  // dynamicImportの書き方その①
+  const dynamicImport = async () => {
+    const module = await import("./add");
+  };
+  dynamicImport();
+
+  // dynamicImportの書き方その②
+  import("./add").then((module) => {
+    console.log(module);
+  });
+  // console.log(add(1, 2));
 };
 
 export default Example;
