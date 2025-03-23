02f4b9ed2afb12f6c23a4dac34caacffbf728ac6
STHEXA <ti.syu.s@gmail.com>
2025-03-23 13:42:14 +0900

    191〜197対応

--- 21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
+++ 21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
@@ -11,3 +11,19 @@ export default async function Detail({ params }) {
     </>
   );
 }
+
+export async function generateStaticParams() {
+  const data = await fetch(ENDPOINT).then((res) => res.json());
+  console.log(data);
+  return data.map((record) => ({ id: record.id }));
+}
+
+export async function generateMetadata({ params }) {
+  const article = await fetch(`${ENDPOINT}/${params.id}`).then((res) =>
+    res.json()
+  );
+  return {
+    title: article.title,
+    description: article.text,
+  };
+}
