bc26b436dd0c2cc1ed00ccd4cde6f97876a31070
STHEXA <ti.syu.s@gmail.com>
2025-05-02 22:11:50 +0900

    関数の型定義の方法

--- 20_typescript/src/080_function_type/start/Example.tsx
+++ 20_typescript/src/080_function_type/start/Example.tsx
@@ -1,5 +1,12 @@
 const Example = () => {
   // 練習用
+  function sum1(x: number, y: number) {
+    return x + y;
+  }
+  const result1 = sum1(1, 2);
+  console.log(result1);
+
+  const sum2 = (x: number, y: number) => x + y;
 };
 
 export default Example;
