f82502444f501dd33337f6ae4c5b353aeba7a512
STHEXA <ti.syu.s@gmail.com>
2025-05-03 22:13:08 +0900

    関数の型定義の方法（戻り値）

--- 20_typescript/src/080_function_type/start/Example.tsx
+++ 20_typescript/src/080_function_type/start/Example.tsx
@@ -1,12 +1,20 @@
 const Example = () => {
   // 練習用
-  function sum1(x: number, y: number) {
-    return x + y;
+  function sum1(x: number, y: number): void {
+    console.log(x + y);
+    // return x + y;
   }
   const result1 = sum1(1, 2);
   console.log(result1);
 
-  const sum2 = (x: number, y: number) => x + y;
+  const sum2 = (x: number, y: number): string => "hello";
+  const result2 = sum2(1, 2);
+  console.log(result2);
+
+  type Sum = (x: number, y: number) => number;
+  const sum3: Sum = (x, y) => x + y;
+  const result3 = sum3(10, 20);
+  console.log(result3);
 };
 
 export default Example;
