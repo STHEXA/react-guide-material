a60b6301078c831dd1fbe78066bc84365a0c8278
STHEXA <ti.syu.s@gmail.com>
2025-05-04 09:42:30 +0900

    useStateの型定義

--- 20_typescript/src/150_usestate/start/Example.tsx
+++ 20_typescript/src/150_usestate/start/Example.tsx
@@ -1,7 +1,22 @@
 import { useState } from "react";
 
+type User = {
+  name: string;
+  age: number;
+};
+
+type Todo = {
+  id: number;
+  text: string;
+};
+
 const Example = () => {
-  
+  const [text, setText] = useState("Hello");
+  useState(0);
+
+  const [animals, setAnimals] = useState<string[]>(["dog", "cat"]);
+  const [users, setUsers] = useState<User[]>([{ name: "tom", age: 23 }]);
+  const [todos, setTodos] = useState<Todo[]>([{ id: 1, text: "Hello" }]);
 };
 
 export default Example;
